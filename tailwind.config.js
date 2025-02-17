9d130452b60bc6bca938f2de6d261f4f6918b014
lamdang0306 <lamdang0306@gmail.com>
2025-02-17 23:28:51 +0700

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,29 @@
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import { defineConfig } from "vite";
 /** @type {import('tailwindcss').Config} */
-export default {
+
+export default defineConfig({
+  base: "/",
+  plugins: [react()],
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+    sourcemap: true, // Generates source maps for debugging
+    minify: "terser", // Uses terser for better minification
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom"], // Separate vendor chunks
+        },
+      },
+    },
   },
   plugins: [],
-};
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"), // Enable @ imports from src directory
+    },
+  },
+});
