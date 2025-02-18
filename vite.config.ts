523949050aa4428fbd49cbcf7378504374ecf9d5
lamdang0306 <lamdang0306@gmail.com>
2025-02-18 11:26:06 +0700

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,9 @@
 import react from "@vitejs/plugin-react";
-import fs from "fs";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    https: {
-      key: fs.readFileSync("localhost-key.pem"),
-      cert: fs.readFileSync("localhost-cert.pem"),
-    },
     host: "0.0.0.0", // Allows access from external networks
     port: 3000, // Use the same port
     strictPort: true,
