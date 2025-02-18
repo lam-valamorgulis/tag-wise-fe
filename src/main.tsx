8e356c579fcc64552d9fc3e823fa595e99121631
lamdang0306 <lamdang0306@gmail.com>
2025-02-18 11:02:24 +0700

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -3,34 +3,35 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { Auth0ProviderWithNavigate } from "./components/Auth0ProviderWithNavigate";
 import "./index.css";
 console.log("Before mounting React"); // Debugging log
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      {/* <Auth0ProviderWithNavigate> */}
-      <ConfigProvider
-        theme={{
-          components: {
-            Typography: {
-              titleMarginBottom: "0",
+      <Auth0ProviderWithNavigate>
+        <ConfigProvider
+          theme={{
+            components: {
+              Typography: {
+                titleMarginBottom: "0",
+              },
+              Table: {
+                borderColor: "rgb(203,213,225)",
+              },
             },
-            Table: {
-              borderColor: "rgb(203,213,225)",
+            token: {
+              fontSize: 14,
+              sizeStep: 4,
             },
-          },
-          token: {
-            fontSize: 14,
-            sizeStep: 4,
-          },
-          algorithm: theme.compactAlgorithm,
-        }}
-      >
-        <App />
-        <h1>hello world</h1>
-      </ConfigProvider>
-      {/* </Auth0ProviderWithNavigate> */}
+            algorithm: theme.compactAlgorithm,
+          }}
+        >
+          <App />
+          {/* <h1>hello world</h1> */}
+        </ConfigProvider>
+      </Auth0ProviderWithNavigate>
     </BrowserRouter>
   </StrictMode>
 );
